a204f76305c9eee431225461aa630e6f611199c0
Mrinal <varshneymrinal2005@gmail.com>
2023-10-11 12:47:48 +0530

    Routes handling

---
+++ routes/chatRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const chatRouter = express.Router(); //to serve the external connection of modules
+const axios = require("axios");
+
+
+// Implement ChatPage APIs here 
+
+module.exports = chatRouter
