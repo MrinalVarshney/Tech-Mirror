55040eb8b3afd4de089c92c90cfe8734af7c7ab9
Mrinal <varshneymrinal2005@gmail.com>
2023-10-11 13:18:46 +0530

    Chat Routes updated

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const chatRouter = express.Router(); //to serve the external connection of modules
-const axios = require("axios");
 
 
 // Implement ChatPage APIs here 
+
+chatRouter.get("/", async (req, res) => {
+    res.render("chatPage");
+});
 
 module.exports = chatRouter
